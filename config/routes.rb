0151356629abb250842f596ff5f5c3765dc78b43
Jacob Marthaller <https://github.com/jmarthaller>
2021-01-16 15:49:17 -0500

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
+  # get 'photos/:id', to: 'photos#show', defaults: { format: 'jpg' }
+
+
+  # get '/posts', to: 'posts#index'
+  # post '/posts', to: "posts#create"
+  # get "/posts/new", to: "posts#new"
+  # get '/posts/:id/edit', to: "posts#edit"
+  # get "/posts/:id", to: "posts#show"
+  # patch "/posts/:id", to: "posts#update"
+  # put "/posts/:id", to: "posts#update"
+
 end
